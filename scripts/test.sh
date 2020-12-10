ae7184ca7fce7076d3371c3db78fcf7a13d84bc8
Urjeet Patel <urjeet@gmail.com>
2020-12-10 00:07:04 -0600

    fix database docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,7 @@
 # Exit in case of error
 set -e
 INSTALL_DEV=true \
-docker-compose \
-    -f docker-compose.yml \
-    config > docker-stack.yml
+docker-compose config > docker-stack.yml
 
 docker-compose -f docker-stack.yml build
 docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
